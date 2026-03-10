9b7a396ea87bc5ee59282a9e702f31f22c8e7f0e
INDavid04 <davidirimia100@gmail.com>
2026-03-10 12:37:27 +0200

    fix: genetici1.cpp base two conversion and interval

--- genetici1.cpp
+++ genetici1.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <string>
 #include <cmath>
+#include <algorithm>
 
 using namespace std;
 
 int main() {
+    string rezultat;
     float a, b; /// capetele intervalului [a, b]
     unsigned int p; /// precizia
     unsigned int m; /// numarul de teste
@@ -26,6 +28,8 @@ int main() {
             float x_i; /// numar real
             cin >> x_i;
             cin.get();
+
+            /// Cauta indexul adica in al catelea interval se afla x_i
             bool caut = true;
             int contor = 1, index = 0;
             while (caut && a + contor * d <= b) {
@@ -37,14 +41,40 @@ int main() {
                 }
             }
             index--;
+
+            /// Converteste index din baza 10 in baza 2
+            // 3/2 = 1 rest 1 si 1/2 = 0 rest 1 de unde rezulta (3)10 = (11)2
+            int index_binar = 0;
+            int putere = 1;
+            while (index) {
+                index_binar = index_binar + putere * (index % 2);
+                index /= 2;
+                putere *= 10;
+            }
+            string str_index_binar = to_string(index_binar);
+            while (str_index_binar.length() < l) {
+                str_index_binar = "0" + str_index_binar;
+            }
+            rezultat += str_index_binar + "\n";
         } else if (directie == "FROM") {
             string sir_binar; /// sir_binar are lungime l
             getline(cin, sir_binar);
-            int l = sir_binar.length();
+
+            /// Converteste sir_binar intr-un numar in baza 10
+            reverse(sir_binar.begin(), sir_binar.end());
+            int putere = 1;
+            int numar = 0;
+            for (char cifra : sir_binar) {
+                numar += putere * (cifra - '0');
+                putere *= 2;
+            }
+            rezultat += to_string(a + numar * d) + "\n";
         } else {
             cout << "Nu ai scris nici TO, nici FROM. Poate ai scris altceva sau nu ai scris cu majuscule.\n";
         }
     }
+
+    cout << rezultat;
     
     return 0;
 }
