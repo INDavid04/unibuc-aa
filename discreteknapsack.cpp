5d7eb43338ef5e94da7d98c34f9177cecfc4201d
INDavid04 <davidirimia100@gmail.com>
2026-03-17 12:49:49 +0200

    fix: functia partitionare

--- discreteknapsack.cpp
+++ discreteknapsack.cpp
@@ -5,14 +5,48 @@
 using namespace std;
 
 int partitionare(float obiect[][3], int stanga, int dreapta) {
+    float temporar, pivot = obiect[dreapta][0];
+    int i = stanga - 1;
 
+    for (int j = stanga; j < dreapta; j++) {
+        if (obiect[j][0] > pivot) {
+            i++;
+
+            temporar = obiect[i][0];
+            obiect[i][0] = obiect[j][0];
+            obiect[j][0] = temporar;
+
+            temporar= obiect[i][1];
+            obiect[i][1] = obiect[j][1];
+            obiect[j][1] = temporar;
+
+            temporar = obiect[i][2];
+            obiect[i][2] = obiect[j][2];
+            obiect[j][2] = temporar;
+        }
+    }
+
+    temporar = obiect[i + 1][0];
+    obiect[i + 1][0] = obiect[dreapta][0];
+    obiect[dreapta][0] = temporar;
+
+    temporar= obiect[i + 1][1];
+    obiect[i + 1][1] = obiect[dreapta][1];
+    obiect[dreapta][1] = temporar;
+
+    temporar = obiect[i + 1][2];
+    obiect[i + 1][2] = obiect[dreapta][2];
+    obiect[dreapta][2] = temporar;
+
+    return i + 1;
 }
 
 void quicksort(float obiect[][3], int stanga, int dreapta) {
-    int st = stanga, dr = dreapta;
-    float pivot = partitionare(obiect, st, dr);
-    quicksort(obiect, stanga, pivot - 1);
-    quicksort(pivot + 1, dreapta);
+    if (stanga < dreapta) {
+        float pivot = partitionare(obiect, stanga, dreapta);
+        quicksort(obiect, stanga, pivot - 1);
+        quicksort(obiect, pivot + 1, dreapta);
+    }
 }
 
 int main() {
@@ -43,18 +77,17 @@ int main() {
     }
 
     /// Calculeaza raportul valoare/greutate
-    for (int = 0; i < numar_obiecte; i++) {
+    for (int i = 0; i < numar_obiecte; i++) {
         obiect[i][0] = obiect[i][1] / obiect[i][2];
     }
 
     /// Sorteaza lista de obiecte dupa raportul valoare/greutate
-    quicksort(&obiect, 0, numar_obiecte);
+    quicksort(obiect, 0, numar_obiecte - 1);
 
     /// Pune obiectele care incap
-    bool incape = true;
     int i = 0, suma = 0;
-    while (incape && i < numar_obiecte) {
-        if (obiect[i][2] < greutate_maxima) {
+    while (i < numar_obiecte) {
+        if (obiect[i][2] <= greutate_maxima) {
             suma += obiect[i][1];
             greutate_maxima -= obiect[i][2];
         }
