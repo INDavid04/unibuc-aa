9d3183537282ba6e780995d6c662d47d10b9e015
INDavid04 <davidirimia100@gmail.com>
2026-04-14 18:45:59 +0300

    feat: reorganize and clean the code

--- t03_genetici.cpp
+++ t03_genetici.cpp
@@ -5,6 +5,7 @@
 /// Attempt #1: Mon, 13 Apr, 2026, 11:22-13:17 ///
 /// Attempt #2: Mon, 13 Apr, 2026, 13:51-14:28 ///
 /// Attempt #3: Mon, 13 Apr, 2026, 14:33-14:50 ///
+/// Attempt #4: Mon, 14 Apr, 2026, 13:25-13:40 ///
 //////////////////////////////////////////////////
 
 #include <iostream> /// cin, cout, of course :)
@@ -15,10 +16,7 @@
 using namespace std;
 
 int main() {
-    /////////////////////////////////
-    /// Declara datele de intrare ///
-    /////////////////////////////////
-
+    /// Datele de intrare
     int numar_cromozomi; /// dimensiunea populatiei
     int capat_stang, capat_drept; /// domeniul de definitie al functiei
     int a, b, c; /// coeficientii polinomului a * x^2 + b * x + c
@@ -27,23 +25,13 @@ int main() {
     double prob_mutatie; /// probabilitatea de mutatie
     int numar_etape; /// numarul de etape al algoritmului
 
-    /////////////////////////////////
-    /// Citeste datele de intrare ///
-    /////////////////////////////////
-
     /// Citim din fisier pentru viteza si afisam in fisier pentru ca s-a cerut
     ifstream fin("t04_genetici.in");
     ofstream fout("t05_genetici.out");
 
     fin >> numar_cromozomi >> capat_stang >> capat_drept >> a >> b >> c >> precizie >> prob_recombinare >> prob_mutatie >> numar_etape;
 
-    //////////////////////////////////
-    /// Afiseaza datele de intrare ///
-    //////////////////////////////////
-
-    fout << "###################\n";
-    fout << "# Date de intrare #\n";
-    fout << "###################\n";
+    fout << "--> Date de intrare";
     fout << "\n";
     fout << "Numar cromozomi: " << numar_cromozomi << "\n";
     fout << "Interval: [" << capat_stang << ", " << capat_drept << "]\n";
@@ -53,24 +41,20 @@ int main() {
     fout << "Probabilitate mutatie: " << prob_mutatie << "\n";
     fout << "Numar etape: " << numar_etape << "\n";
 
-    ///////////////////////////////////////////////////
-    /// Calculeaza numarul de biti al unui cromozom ///
-    ///////////////////////////////////////////////////
+    //////////////////////////
+    /// Populatia initiala ///
+    //////////////////////////
 
+    /// Calculeaza numarul de biti al unui cromozom 
     /// Pentru intervalul [capat_stang, capat_drept] si o precizie de precizie zecimale:
     double l = log2((capat_drept - capat_stang) * pow(10, precizie));
     int numar_biti = ceil(l);
     fout << "\n";
-    fout << "###################\n";
-    fout << "# Date aditionale #\n";
-    fout << "###################\n";
+    fout << "--> Date aditionale";
     fout << "\n";
     fout << "Numar biti: " << numar_biti << "\n";
 
-    /////////////////////////////////////////
-    /// Genereaza cromozomii la nimereala ///
-    /////////////////////////////////////////
-
+    /// Genereaza cromozomii la nimereala
     int cromozom_binar[numar_cromozomi][numar_biti]; /// reprezentarea binara
     for (int i = 0; i < numar_cromozomi; i++) {
         for (int j = 0; j < numar_biti; j++) {
@@ -78,14 +62,9 @@ int main() {
         }
     }
 
-    /////////////////////////////////////////////////////
-    /// Afiseaza reprezentarea pe biti a cromozomului ///
-    /////////////////////////////////////////////////////
-
+    /// Afiseaza reprezentarea pe biti a cromozomului
     fout << "\n";
-    fout << "########################################\n";
-    fout << "# Reprezentarea pe biti a cromozomului #\n";
-    fout << "########################################\n";
+    fout << "--> Reprezentarea pe biti a cromozomului";
     fout << "\n";
 
     for (int i = 0; i < numar_cromozomi; i++) {
@@ -99,14 +78,9 @@ int main() {
         fout << "\n";
     }
 
-    /////////////////////////////////////////////////////////////////
-    /// Afiseaza valoarea corespunzatoare cromozomului in domeniu ///
-    /////////////////////////////////////////////////////////////////
-
+    /// Afiseaza valoarea corespunzatoare cromozomului in domeniu
     fout << "\n";
-    fout << "####################################################\n";
-    fout << "# Valoarea corespunzatoare cromozomului in domeniu #\n";
-    fout << "####################################################\n";
+    fout << "--> Valoarea corespunzatoare cromozomului in domeniu";
     fout << "\n";
 
     /// Converteste din binar in zecimal
@@ -129,14 +103,9 @@ int main() {
         fout << i + 1 << ": " << cromozom_real[i] << "\n";
     }
 
-    /////////////////////////////////////////////////////////////////
-    /// Afiseaza valoarea corespunzatoare cromozomului in domeniu ///
-    /////////////////////////////////////////////////////////////////
-
+    /// Afiseaza valoarea corespunzatoare cromozomului in domeniu
     fout << "\n";
-    fout << "#############################################\n";
-    fout << "# Valoarea functiei in punctul cromozomului #\n";
-    fout << "#############################################\n";
+    fout << "--> Valoarea functiei in punctul cromozomului";
     fout << "\n";
 
     double fitness[numar_cromozomi]; /// valoarea functiei in punctul din domeniu care corespunde cromozomului
@@ -148,15 +117,11 @@ int main() {
         }
         fout << i + 1 << ": " << fitness[i] << "\n";
     }
-
-    /////////////////////////////////////////////////////////////////
-    /// Afiseaza valoarea corespunzatoare cromozomului in domeniu ///
-    /////////////////////////////////////////////////////////////////
-
+    
     fout << "\n";
-    fout << "##########################\n";
-    fout << "### Populatia initiala ###\n";
-    fout << "##########################\n";
+    fout << "######################\n";
+    fout << "# Populatia initiala #\n";
+    fout << "######################\n";
     fout << "\n";
 
     for (int i = 0; i < numar_cromozomi; i++) {
