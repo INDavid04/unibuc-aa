fb421c62683ffa03a57998fbde2208addb9e2e24
INDavid04 <davidirimia100@gmail.com>
2026-05-25 16:53:54 +0300

    fix: determinant calculat corect cu paranteze la adunari

--- l7pb1.cpp
+++ l7pb1.cpp
@@ -8,7 +8,7 @@ struct Punct { int x, y; };
 
 int main() {
     /// Citeste coordonatele punctelor triunghiului
-    int xA, yA, xB, yB, xC, yC;
+    long long xA, yA, xB, yB, xC, yC;
     cin >> xA >> yA;
     cin >> xB >> yB;
     cin >> xC >> yC;
@@ -27,8 +27,8 @@ int main() {
     for (int i = 0; i < m; i++) {
         /// inside (d > 0), outside (d < 0), boundary (d == 0)
         long long d = 0; 
-        int xD = P[i].x;
-        int yD = P[i].y;
+        long long xD = P[i].x;
+        long long yD = P[i].y;
 
         /// Pentru A, B, C, D, determinantul este
         /// | xA yA xA*xA+yA*yA 1 |
@@ -46,10 +46,10 @@ int main() {
         /// - | xC yC xC*xC+yC*yC | + | xC yC xC*xC+yC*yC | - | xB yB xB*xB+yB*yB | + | xB yB xB*xB+yB*yB |
         ///   | xD yD xD*xD+yD*yD |   | xD yD xD*xD+yD*yD |   | xD yD xD*xD+yD*yD |   | xC yC xC*xC+yC*yC |
 
-        d -= xB*yC*xD*xD+yD*yD + xC*yD*xB*xB+yB*yB + xD*xC*xC+yC*yC*yB - xB*xB+yB*yB*yC*xD - xC*xC+yC*yC*yD*xB - xD*xD+yD*yD*xC*yB;
-        d += xA*yC*xD*xD+yD*yD + xC*yD*xA*xA+yA*yA + xD*xC*xC+yC*yC*yA - xA*xA+yA*yA*yC*xD - xC*xC+yC*yC*yD*xA - xD*xD+yD*yD*xC*yA;
-        d -= xA*yB*xD*xD+yD*yD + xB*yD*xA*xA+yA*yA + xD*xB*xB+yB*yB*yA - xA*xA+yA*yA*yB*xD - xB*xB+yB*yB*yD*xA - xD*xD+yD*yD*xB*yA;
-        d += xA*yB*xC*xC+yC*yC + xB*yC*xA*xA+yA*yA + xC*xB*xB+yB*yB*yA - xA*xA+yA*yA*yB*xC - xB*xB+yB*yB*yC*xA - xC*xC+yC*yC*xB*yA;
+        d -= xB*yC*(xD*xD+yD*yD) + xC*yD*(xB*xB+yB*yB) + xD*(xC*xC+yC*yC)*yB - (xB*xB+yB*yB)*yC*xD - (xC*xC+yC*yC)*yD*xB - (xD*xD+yD*yD)*xC*yB;
+        d += xA*yC*(xD*xD+yD*yD) + xC*yD*(xA*xA+yA*yA) + xD*(xC*xC+yC*yC)*yA - (xA*xA+yA*yA)*yC*xD - (xC*xC+yC*yC)*yD*xA - (xD*xD+yD*yD)*xC*yA;
+        d -= xA*yB*(xD*xD+yD*yD) + xB*yD*(xA*xA+yA*yA) + xD*(xB*xB+yB*yB)*yA - (xA*xA+yA*yA)*yB*xD - (xB*xB+yB*yB)*yD*xA - (xD*xD+yD*yD)*xB*yA;
+        d += xA*yB*(xC*xC+yC*yC) + xB*yC*(xA*xA+yA*yA) + xC*(xB*xB+yB*yB)*yA - (xA*xA+yA*yA)*yB*xC - (xB*xB+yB*yB)*yC*xA - (xC*xC+yC*yC)*xB*yA;
 
         if (d > 0) {
             cout << "INSIDE\n";
